4f17e90e90fbbef8e8eddaaa0418a58468a018f1
Clever-Shadow <los.zhenia1698@yandex.ru>
2025-04-03 15:47:17 +0400

    #2 Add gitignore

--- TextEditorApp/obj/Debug/net9.0-windows/TextEditorApp.AssemblyInfo.cs
+++ TextEditorApp/obj/Debug/net9.0-windows/TextEditorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextEditorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e594986f4e8733d453f9e1f763135decb04408c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextEditorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TextEditorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
